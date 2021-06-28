ae1b3467928c4359a85e1bcd0ba6b71ba79ced98
IAmSharcBait <IAmSharcBait@github.com>
2021-06-27 18:31:00 -0700

    Continuing to add the basic structure of the page

--- app.js
+++ app.js
@@ -4,32 +4,25 @@
 const switcher = document.querySelector('.btn'); /* gets the reference to the theme changing button */
 var projects = 0;
 
-function addSticky () {
+function addSticky (id) {
     // create a new div element
     const newDiv = document.createElement("div");
     newDiv.className = "sticky";
     newDiv.id = projects+1;
     
     const x = document.createElement("section");
-    x.className = "section";
-    x.id = "tier-name";
-    x.contentEditable = "true";
-    x.innerText = "tier"
-
-    const y = document.createElement("section");
-    y.className = "section";
-    y.id = "buttons";
-    y.innerText = "btns here"
+    x.className = "project";
+    x.id = "project-name";
+    x.innerText = "project-name"
 
     newDiv.appendChild(x);
-    newDiv.appendChild(y);
 
     // add the newly created element and its content into the DOM
-    document.getElementById("tierlist").appendChild(newDiv);
+    document.getElementById(id).appendChild(newDiv);
 };
 
 function deleteSticky () {
-    var row = document.getElementById(num_rows);
+    var row = document.getElementById(projects);
     row.remove();
-    num_rows = num_rows - 1;
+    projects = projects - 1;
 };
